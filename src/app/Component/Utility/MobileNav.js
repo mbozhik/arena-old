8b8da5b82197ab814309d0662c3ea877e87776a1
MD Shaharia <adeptshaharia@gmail.com>
2024-02-08 17:57:19 +0600

    Hober_banner_responsive

--- src/app/Component/Utility/MobileNav.js
+++ src/app/Component/Utility/MobileNav.js
@@ -1,169 +1,221 @@
-'use client'
-import React, { useState } from 'react';
-import { AiOutlineDown, AiOutlineMenu } from 'react-icons/ai';
-import LogOut from './Logout';
-import Link from 'next/link';
+"use client";
+import React, { useState } from "react";
+import { AiOutlineDown, AiOutlineMenu } from "react-icons/ai";
+import LogOut from "./Logout";
+import Link from "next/link";
 import Logo from "@/app/Images/Banner/Logo.png";
-import Image from 'next/image';
+import Image from "next/image";
 import { IoIosCloseCircle } from "react-icons/io";
 
+const MobileNav = ({ uid }) => {
+  const [nav, setNave] = useState(false);
 
-const MobileNav = ({uid}) => {
-    const [nav,setNave]=useState(false);
-
-   
-    return (
-        <div className="md:flex navber lg:px-10 md:px-5 relative  ">
-        <article>
-          <Image
-            src={Logo}
-            alt="Arena-Logo"
-            className="w-20 h-10 md:w-auto md:h-auto "
-          />
-        </article>
-        <div className={`${nav?"h-full w-full blur fixed top-0 left-0":""} md:hidden z-10`}></div>
-        <nav className="flex justify-center items-center  ">
-          <ul className={`md:flex block peer z-50  text-white ${nav?'translate-x-0 md:translate-x-0' :'translate-x-96 md:translate-x-0 '} transition-all duration-350 ease-in-out font-semibold gap-x-4 bg-[#331E6D] md:bg-transparent fixed md:relative lg:relative top-0 right-0 h-screen md:h-auto    w-2/3 md:w-auto pt-10 pl-5 md:pt-0 md:pl-0 `}>
-          <div className='block md:hidden text-white absolute right-5 top-3 text-3xl'><IoIosCloseCircle onClick={()=>setNave(false)}></IoIosCloseCircle></div>
-            <li className="hover:text-[#ffd321] mt-5 md:mt-0" onClick={()=>setNave(false)}>
-              {" "}
-              <Link href="/">Home</Link>
-            </li>
-            <li className="hover:text-[#ffd321] mt-2 md:mt-0"onClick={()=>setNave(false)}>
-              <Link href="/about">About</Link>
-            </li>
-            <div className="  group mt-2 md:mt-0 ">
-              <span className="hover:text-[#ffd321]  " >
-           
-                <p className="hidden md:inline-flex relative"> Courses</p>
-                <span className="flex md:hidden lg:hidden items-center gap-3">
-       
-                  Courses
-                  <AiOutlineDown className="text-white block md:hidden" />
-                </span>
-              </span>
-              <div className="  w-full group-hover:h-48 block md:hidden  group-hover:opacity-100 h-0 overflow-hidden z-0    duration-500 ease-in-out transition-all text-[12px] ml-5">
-                <div className="md:py-5 md:px-10">
-                  <li className="hover:text-[#ffd321] cursor-pointer " onClick={()=>setNave(false)}>
-                    <Link href="/cybersecurity">
-                      Cyber Security & Ethical Hacking
-                    </Link>
-                  </li>
-                  <li className="hover:text-[#ffd321] cursor-pointer my-1 "onClick={()=>setNave(false)}>
-                    <Link href="/python">Advanced Python With Freelancing</Link>
-                  </li>
-                  <li className="hover:text-[#ffd321] cursor-pointer my-1"onClick={()=>setNave(false)}>
-                    <Link href="/combo">
-                      Cyber Security and Advance Python (Combo)
-                    </Link>
-                  </li>
-                  <li className="hover:text-[#ffd321] cursor-pointer my-1"onClick={()=>setNave(false)}>
-                    <Link href="/cehf">CEHF Masterclass</Link>
-                  </li>
-                  <li className="hover:text-[#ffd321] cursor-pointer my-1"onClick={()=>setNave(false)}>
-                    <Link href="/cosint">
-                      Certified in Open Source Intelligence (C|OSINT)
-                    </Link>
-                  </li>
-                  <li className="hover:text-[#ffd321] cursor-pointer my-1"onClick={()=>setNave(false)}>
-                    <Link href="/linux">Linux</Link>
-                  </li>
-                  <li className="hover:text-[#ffd321] cursor-pointer my-1"onClick={()=>setNave(false)}>
-                    <Link href="/network">Networking Security</Link>
-                  </li>
-                </div>
-              </div>
-              <br className="hidden md:block mt-2 md:mt-0" onClick={()=>setNave(false)}/>
-              <Link
-                href="/courses "
-                className="hover:text-[#ffd321] absolute pr-6 opacity-0 hidden md:block  "
-              >
+  return (
+    <div className="md:flex navber lg:px-10 md:px-5 relative  ">
+      <article>
+        <Image
+          src={Logo}
+          alt="Arena-Logo"
+          className="w-20 h-10 md:w-auto md:h-auto "
+        />
+      </article>
+      <div
+        className={`${
+          nav ? "h-full w-full blur fixed top-0 left-0" : ""
+        } md:hidden z-10`}
+      ></div>
+      <nav className="flex justify-center items-center  ">
+        <ul
+          className={`md:flex block peer z-50  text-white ${
+            nav
+              ? "translate-x-0 md:translate-x-0"
+              : "translate-x-96 md:translate-x-0 "
+          } transition-all duration-350 ease-in-out font-semibold gap-x-4 bg-[#331E6D] md:bg-transparent fixed md:relative lg:relative top-0 right-0 h-screen md:h-auto    w-2/3 md:w-auto pt-10 pl-5 md:pt-0 md:pl-0 `}
+        >
+          <div className="block md:hidden text-white absolute right-5 top-3 text-3xl">
+            <IoIosCloseCircle onClick={() => setNave(false)}></IoIosCloseCircle>
+          </div>
+          <li
+            className="hover:text-[#ffd321] mt-5 md:mt-0"
+            onClick={() => setNave(false)}
+          >
+            {" "}
+            <Link href="/">Home</Link>
+          </li>
+          <li
+            className="hover:text-[#ffd321] mt-2 md:mt-0"
+            onClick={() => setNave(false)}
+          >
+            <Link href="/about">About</Link>
+          </li>
+          <div className="  group mt-2 md:mt-0 ">
+            <span className="hover:text-[#ffd321]  ">
+              <p className="hidden md:inline-flex relative"> Courses</p>
+              <span className="flex md:hidden lg:hidden items-center gap-3">
                 Courses
-              </Link>
-              <div className="absolute hidden md:block  mt-5 w-96 group-hover:h-72  group-hover:opacity-100 h-0 overflow-hidden z-0 bg-[#678cc9]   duration-500 ease-in-out transition-all">
-                <div className="md:py-5 md:px-10">
-                  <li className="hover:text-[#ffd321] cursor-pointer ">
-                    <Link href="/cybersecurity">
-                      Cyber Security & Ethical Hacking
-                    </Link>
-                  </li>
-                  <li className="hover:text-[#ffd321] cursor-pointer my-1 ">
-                    <Link href="/python">Advanced Python With Freelancing</Link>
-                  </li>
-                  <li className="hover:text-[#ffd321] cursor-pointer my-1">
-                    <Link href="/combo">
-                      Cyber Security and Advance Python (Combo)
-                    </Link>
-                  </li>
-                  <li className="hover:text-[#ffd321] cursor-pointer my-1">
-                    <Link href="/cehf">CEHF Masterclass</Link>
-                  </li>
-                  <li className="hover:text-[#ffd321] cursor-pointer my-1">
-                    <Link href="/cosint">
-                      Certified in Open Source Intelligence (C|OSINT)
-                    </Link>
-                  </li>
-                  <li className="hover:text-[#ffd321] cursor-pointer my-1">
-                    <Link href="/linux">Linux</Link>
-                  </li>
-                  <li className="hover:text-[#ffd321] cursor-pointer my-1">
-                    <Link href="/network">Networking Security</Link>
-                  </li>
-                </div>
+                <AiOutlineDown className="text-white block md:hidden" />
+              </span>
+            </span>
+            <div className="  w-full group-hover:h-48 block md:hidden  group-hover:opacity-100 h-0 overflow-hidden z-0    duration-500 ease-in-out transition-all text-[12px] ml-5">
+              <div className="md:py-5 md:px-10">
+                <li
+                  className="hover:text-[#ffd321] cursor-pointer "
+                  onClick={() => setNave(false)}
+                >
+                  <Link href="/cybersecurity">
+                    Cyber Security & Ethical Hacking
+                  </Link>
+                </li>
+                <li
+                  className="hover:text-[#ffd321] cursor-pointer my-1 "
+                  onClick={() => setNave(false)}
+                >
+                  <Link href="/python">Advanced Python With Freelancing</Link>
+                </li>
+                <li
+                  className="hover:text-[#ffd321] cursor-pointer my-1"
+                  onClick={() => setNave(false)}
+                >
+                  <Link href="/combo">
+                    Cyber Security and Advance Python (Combo)
+                  </Link>
+                </li>
+                <li
+                  className="hover:text-[#ffd321] cursor-pointer my-1"
+                  onClick={() => setNave(false)}
+                >
+                  <Link href="/cehf">CEHF Masterclass</Link>
+                </li>
+                <li
+                  className="hover:text-[#ffd321] cursor-pointer my-1"
+                  onClick={() => setNave(false)}
+                >
+                  <Link href="/cosint">
+                    Certified in Open Source Intelligence (C|OSINT)
+                  </Link>
+                </li>
+                <li
+                  className="hover:text-[#ffd321] cursor-pointer my-1"
+                  onClick={() => setNave(false)}
+                >
+                  <Link href="/linux">Linux</Link>
+                </li>
+                <li
+                  className="hover:text-[#ffd321] cursor-pointer my-1"
+                  onClick={() => setNave(false)}
+                >
+                  <Link href="/network">Networking Security</Link>
+                </li>
               </div>
             </div>
-            <li className="hover:text-[#ffd321]  md:mt-0">
-              <Link href="/services" onClick={()=>setNave(false)}>Services</Link>
+            <br
+              className="hidden md:block mt-2 md:mt-0"
+              onClick={() => setNave(false)}
+            />
+            <Link
+              href="/courses "
+              className="hover:text-[#ffd321] absolute pr-6 opacity-0 hidden md:block  "
+            >
+              Courses
+            </Link>
+            <div className="absolute hidden md:block  mt-5 w-96 group-hover:h-72  group-hover:opacity-100 h-0 overflow-hidden z-0 bg-[#678cc9]   duration-500 ease-in-out transition-all">
+              <div className="md:py-5 md:px-10">
+                <li className="hover:text-[#ffd321] cursor-pointer ">
+                  <Link href="/cybersecurity">
+                    Cyber Security & Ethical Hacking
+                  </Link>
+                </li>
+                <li className="hover:text-[#ffd321] cursor-pointer my-1 ">
+                  <Link href="/python">Advanced Python With Freelancing</Link>
+                </li>
+                <li className="hover:text-[#ffd321] cursor-pointer my-1">
+                  <Link href="/combo">
+                    Cyber Security and Advance Python (Combo)
+                  </Link>
+                </li>
+                <li className="hover:text-[#ffd321] cursor-pointer my-1">
+                  <Link href="/cehf">CEHF Masterclass</Link>
+                </li>
+                <li className="hover:text-[#ffd321] cursor-pointer my-1">
+                  <Link href="/cosint">
+                    Certified in Open Source Intelligence (C|OSINT)
+                  </Link>
+                </li>
+                <li className="hover:text-[#ffd321] cursor-pointer my-1">
+                  <Link href="/linux">Linux</Link>
+                </li>
+                <li className="hover:text-[#ffd321] cursor-pointer my-1">
+                  <Link href="/network">Networking Security</Link>
+                </li>
+              </div>
+            </div>
+          </div>
+          <li className="hover:text-[#ffd321]  md:mt-0">
+            <Link href="/services" onClick={() => setNave(false)}>
+              Services
+            </Link>
+          </li>
+          <li className="hover:text-[#ffd321] mt-2 md:mt-0">
+            <Link
+              href="https://www.hackers.institute/"
+              onClick={() => setNave(false)}
+            >
+              Blog
+            </Link>
+          </li>
+          <li className="hover:text-[#ffd321] mt-2 md:mt-0">
+            <Link href="/gallery" onClick={() => setNave(false)}>
+              Gallery
+            </Link>
+          </li>{" "}
+          {uid && (
+            <li className="hover:text-[#ffd321]">
+              <Link href="/profile" onClick={() => setNave(false)}>
+                Profile
+              </Link>
             </li>
-            <li className="hover:text-[#ffd321] mt-2 md:mt-0">
-              <Link href="https://www.hackers.institute/"onClick={()=>setNave(false)}>Blog</Link>
+          )}
+          {uid ? (
+            <li className="mt-5 " onClick={() => setNave(false)}>
+              {" "}
+              {/* <span className="navBtn " >Log Out</span> */}
+              <LogOut />
             </li>
-            <li className="hover:text-[#ffd321] mt-2 md:mt-0">
-              <Link href="/gallery"onClick={()=>setNave(false)}>Gallery</Link>
-            </li>{" "}
-            {uid && (
-              <li className="hover:text-[#ffd321]">
-                <Link href="/profile"onClick={()=>setNave(false)}>Profile</Link>
-              </li>
-            )}
-               {uid ? (
-              <li className='mt-5 'onClick={()=>setNave(false)}>
+          ) : (
+            <li className="mt-5 block md:hidden" onClick={() => setNave(false)}>
+              <Link href="/admission" className="">
                 {" "}
-                {/* <span className="navBtn " >Log Out</span> */}
-                <LogOut />
-              </li>
-            ) : (
-              <li className='mt-5 block md:hidden'onClick={()=>setNave(false)}>
-                <Link href="/admission" className="">
-                  {" "}
-                  <span className="navBtn ">Apply Now</span>
-                </Link>
-              </li>
-            )}
-          </ul>
-        </nav>
-        <article className=" justify-center items-center hidden md:flex ">
-          <ul className="flex text-white font-semibold gap-x-4">
-            {uid ? (
-              <li>
+                <span className="navBtn ">Apply Now</span>
+              </Link>
+            </li>
+          )}
+        </ul>
+      </nav>
+      <article className=" justify-center items-center hidden md:flex ">
+        <ul className="flex text-white font-semibold gap-x-4">
+          {uid ? (
+            <li>
+              {" "}
+              <LogOut />
+            </li>
+          ) : (
+            <li>
+              <Link href="/admission" className="">
                 {" "}
-                <LogOut />
-              </li>
-            ) : (
-              <li>
-                <Link href="/admission" className="">
-                  {" "}
-                  <span className="navBtn ">Apply Now</span>
-                </Link>
-              </li>
-            )}
-          </ul>
-        </article>
-        <div className="  justify-center   relative flex items-center md:hidden" onClick={()=>setNave(true)}>
-          <AiOutlineMenu className="text-white text-2xl menu-bar"  />
-        </div>
+                <span className="navBtn ">Apply Now</span>
+              </Link>
+            </li>
+          )}
+        </ul>
+      </article>
+      <div
+        className="  justify-center   relative flex items-center md:hidden"
+        onClick={() => setNave(true)}
+      >
+        <AiOutlineMenu className="text-white text-2xl menu-bar" />
       </div>
-    );
+    </div>
+  );
 };
 
 export default MobileNav;
--- src/app/Component/Utility/NavState.js
+++
@@ -1,8 +0,0 @@
-let nav =true;
-function ChangeNav(){
-   let newNav= !nav
-   nav =newNav;
-   console.log(nav);
-    return  nav;
-}
-export {nav,ChangeNav}
