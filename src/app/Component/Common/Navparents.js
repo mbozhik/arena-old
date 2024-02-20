a0306c4c46f2294990efaae81c9fe99e3a319d5b
MD Shaharia <adeptshaharia@gmail.com>
2024-02-20 16:09:49 +0600

    remove_messenger

--- src/app/Component/Common/Navparents.js
+++ src/app/Component/Common/Navparents.js
@@ -4,7 +4,7 @@
 import Marque from "../Utility/Marque";
 import MobileNav from "../Utility/MobileNav";
 import { usePathname } from 'next/navigation'
-import MessengerCustomerChat from 'react-messenger-customer-chat';
+// import MessengerCustomerChat from 'react-messenger-customer-chat';
 
 const Navparents = ({data,uid}) => {
     const pathname = usePathname()
@@ -12,8 +12,8 @@ const Navparents = ({data,uid}) => {
     return (
   
         <div className={`${pathname==='/'?'fixed':'sticky'} w-full !top-0 z-50` }>
-              <MessengerCustomerChat  pageId="1422013914563781"
-                    appId="308111384934899"  />
+              {/* <MessengerCustomerChat  pageId="1422013914563781"
+                    appId="308111384934899"  /> */}
       <Marque data={data}/>
       <MobileNav uid={uid}/>
     </div>
