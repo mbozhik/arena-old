49f5131247732feb3e79a2224eb9de2164f566f8
MD Shaharia <adeptshaharia@gmail.com>
2024-02-20 15:38:42 +0600

    --peeer

--- src/app/Services/page.js
+++ src/app/Services/page.js
@@ -2,7 +2,7 @@ import { AllServices } from "../Component/Services/AllServices";
 
 export const metadata = {
     title: 'Cybersecurity & Tech Courses | Arena Web  Security',
-    description: ' We are offering courses in cyber security, ethical hacking, OSINT, Linux, software development, and networking security.',
+    description: ' We are offering courses in cyber security, ethical hacking, O|SINT, Linux, software development, and networking security.',
   }; 
 const Page = () => {
     return (
