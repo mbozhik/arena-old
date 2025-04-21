76bd7d6ff9343b9557e5db08f1b6767442c61704
shaharia <adeptshaharia@gmail.com>
2025-04-22 02:16:50 +0600

    statitis

--- src/app/Component/home/CourseStatistic.js
+++ src/app/Component/home/CourseStatistic.js
@@ -8,7 +8,7 @@ const CourseStatistic = () => {
       {/* <div className="CourseStatistic"></div> */}
       <div className="text-center bg-white ">
       <h5 className="text-center md:text-[24px] xl:text-[26px] font-bold text-[#0b6481]  mb-2"> 
-      Company Statitic
+      Company Statistics
       </h5>
         {/* <h1 className="font-bold md:text-3xl text-[#0b6481]"></h1> */}
         {/* <p className="lg:w-56 bg-[#ffd321] h-1 "></p> */}
