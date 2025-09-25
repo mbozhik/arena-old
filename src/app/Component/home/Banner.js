6d939dd706889ef79970b610e0d10a0dd38080fd
maxim <bozzhik@gmail.com>
2025-09-25 16:28:28 +0300

    implement edits for `Banner` component

--- src/app/Component/home/Banner.js
+++ src/app/Component/home/Banner.js
@@ -5,13 +5,13 @@ import {useState, useEffect} from 'react'
 
 const banners = [
   {
-    src: '/banner/aws-banner1.png',
+    src: '/banner/banner1.png',
     title: 'Arena Web Security',
     subtitle: 'New window of World Wide Web',
     description: 'Arena Web Security is the first and most trusted cyber security company in Bangladesh, established in 2012. Over the years, Arena has provided advanced Cyber Security Services and Professional Training to clients across the globe - including the Government and Ministries of Bangladesh, Military, Banking Sector, Educational Institutions, and Private Organizations.',
   },
   {
-    src: '/banner/aws-banner2.webp',
+    src: '/banner/banner2.webp',
     title: 'Arena Web Security',
     subtitle: 'Hacking was passion, Security is our profession',
     description: 'Arena Web Security is the first and most trusted cyber security company in Bangladesh, established in 2012. Over the years, Arena has provided advanced Cyber Security Services and Professional Training to clients across the globe - including the Government and Ministries of Bangladesh, Military, Banking Sector, Educational Institutions, and Private Organizations.',
@@ -29,14 +29,16 @@ export default function CarouselComponent() {
   }, [])
 
   return (
-    <div className="relative overflow-hidden xl:h-[60vh] md:h-[30vh] h-[36vh] ">
+    <div className="relative overflow-hidden xl:h-[65vh] md:h-[35vh] h-[36vh]">
       {banners.map((banner, index) => (
-        <div key={index} className={`absolute top-0 left-0 w-full h-full transition-opacity duration-1000 ease-in-out ${index === activeIndex ? 'opacity-100 z-10' : 'opacity-0 z-0'}`}>
+        <div key={index} className={`absolute top-0 left-0 w-full h-full transition-opacity duration-1000 ease-in-out text-[#1F202F] ${index === activeIndex ? 'opacity-100 z-10' : 'opacity-0 z-0'}`}>
           <img src={banner.src} alt="slide" className="object-cover w-full h-full TopBanner" />
           <div className="container absolute inset-0 flex flex-col items-center justify-center p-4 mx-auto text-center md:px-10 xl:px-0">
-            <h4 className="text-[28px] md:text-[42px] xl:text-[44px] md:leading-[50px] xl:leading-[53px] font-bold">{banner.title}</h4>
-            <p className="text-[18px] md:text-[26px] xl:text-[20px] leading-7 mt-1">{banner.subtitle}</p>
-            <p className=" text-[11px] md:text-[16px] text-gray-800 mt-4">{banner.description}</p>
+            <h4 className="text-[28px] md:text-[42px] xl:text-[56px] md:leading-[50px] xl:leading-[53px] font-bold">{banner.title}</h4>
+            <p className="text-[18px] md:text-[26px] xl:text-[22px] leading-7 mt-1.5 font-light">{banner.subtitle}</p>
+
+            <div className="h-0.5 bg-[#1F202F] w-[15%] my-6"></div>
+            <p className="text-[11px] md:text-[16px] max-w-[85ch] font-light">{banner.description}</p>
           </div>
         </div>
       ))}
