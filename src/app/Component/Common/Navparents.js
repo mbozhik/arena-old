17e7cf807fed269ba8b7b8e2e07f21eda8f178c1
maxim <bozzhik@gmail.com>
2025-11-21 19:26:23 +0300

    fix `NavParents` common component

--- src/app/Component/Common/Navparents.js
+++ src/app/Component/Common/Navparents.js
@@ -15,9 +15,8 @@ const Navparents = ({data, uid}) => {
                     appId="308111384934899"  /> */}
       <Marque data={data} />
       <div className="px-4 py-2 mx-auto ">
-        <div className="flex flex-wrap mt-2 mb-2">
-          <div className="flex justify-center w-full px-4 xl:w-1/3 lg:w-1/3 md:w-1/3 sm:w-full"></div>
-          <div className="flex items-center justify-center w-full px-4 xl:w-1/3 lg:w-1/3 md:w-1/3 sm:w-full">
+        <div className="grid grid-cols-1 gap-2 mt-2 mb-2 lg:grid-cols-3">
+          <div className="flex items-center justify-center w-full col-span-1 px-4 md:col-start-2 md:col-span-1 lg:col-span-2">
             <Link href="/" className="flex items-center gap-4">
               <img
                 className="mx-auto w-52"
@@ -34,7 +33,7 @@ const Navparents = ({data, uid}) => {
               />
             </Link>
           </div>
-          <div className="flex items-center justify-center w-full xl:w-1/3 lg:w-1/3 md:w-1/3 sm:w-full md:justify-end md:pr-20 ">
+          <div className="flex items-center justify-center w-full col-span-1 lg:justify-end md:pr-20">
             <a
               target="_blank"
               href="https://www.facebook.com/ArenaBangladesh/"
